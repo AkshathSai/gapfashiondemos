3b0c9050beed7feb17485ede6b488a18a026dca8
Akshath Sai, Pittala <akshathsai.pittala@outlook.com>
2025-07-21 18:28:52 +0530

    feat: add validation for quantity in AddToCartRequest

--- ecommerceapp/src/main/java/com/gap/ecommerceapp/dto/AddToCartRequest.java
+++ ecommerceapp/src/main/java/com/gap/ecommerceapp/dto/AddToCartRequest.java
@@ -1,10 +1,12 @@
 package com.gap.ecommerceapp.dto;
 
+import jakarta.validation.constraints.Min;
 import lombok.Data;
 
 @Data
 public class AddToCartRequest {
     private Long userId;
     private Long productId;
+    @Min(value = 1, message = "Quantity must be at least 1")
     private Integer quantity;
 }
